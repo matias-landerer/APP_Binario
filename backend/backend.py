7896b3788c10c61756910d80c26317b130fbcae6
Matías Landerer <lanconts7@gmail.com>
2025-07-17 16:50:33 -0400

    Backend y Frontend actualizados

--- backend/backend.py
+++ backend/backend.py
@@ -1,44 +1,62 @@
 import transformador
+from PyQt5.QtCore import QObject, pyqtSignal
 
-def decimal(deci: str) -> None:
-    try:
-        if not deci.isdigit():
-            raise ValueError('Número decimal inválido.')
-        
-        bina = transformador.decimal_a_binario(deci)
-        hexa = transformador.decimal_a_hexadecimal(deci)
-
-        print(f'\nBinario: {bina}\nHexadecimal: {hexa}')
+class Transformador(QObject):
+    senal_resultado = pyqtSignal(tuple)
     
-    except ValueError as error:
-        print(error)
+    def __init__(self) -> None:
+        super().__init__()
 
-def binario(bina: str) -> None:
-    try:
-        for i in bina:
-            if i not in ['0', '1']:
-                raise ValueError('Número binario inválido.')
-        
-        deci = transformador.binario_a_decimal(bina)
-        hexa = transformador.binario_a_hexadecimal(bina)
-
-        print(f'\nDecimal: {deci}\nHexadecimal: {hexa}')
-
-    except ValueError as error:
-        print(error)
-
-def hexadecimal(hexa: str) -> None:
-    try:
-        if (len(hexa) < 3) or (hexa[:2] != '0x' and hexa[:2] != '0X'):
-            raise ValueError('Hexadecimal inválido.')
-        
-        deci = transformador.hexadecimal_a_decimal(hexa)
-        bina = transformador.hexadecimal_a_binario(hexa)
-
-        print(f'\nDecimal: {deci}\nBinario: {bina}')
+    def selector(self, mensaje: str, numero: str) -> None:
+        if mensaje == 'Decimal':
+            self.decimal(numero)
+        elif mensaje == 'Binario':
+            self.binario(numero)
+        elif mensaje == 'Hexadecimal':
+            self.hexadecimal(numero)
     
-    except ValueError as error:
-        print(error)
+    def decimal(self, deci: str) -> None:
+        try:
+            if not deci.isdigit():
+                raise ValueError('Número decimal inválido.')
+            
+            bina = transformador.decimal_a_binario(deci)
+            hexa = transformador.decimal_a_hexadecimal(deci)
+
+            print(f'\nBinario: {bina}\nHexadecimal: {hexa}')
+            self.senal_resultado((f'Binario: {bina}', 'Hexadecimal: {hexa}'))
+        
+        except ValueError as error:
+            print(error)
+
+    def binario(self, bina: str) -> None:
+        try:
+            for i in bina:
+                if i not in ['0', '1']:
+                    raise ValueError('Número binario inválido.')
+            
+            deci = transformador.binario_a_decimal(bina)
+            hexa = transformador.binario_a_hexadecimal(bina)
+
+            print(f'\nDecimal: {deci}\nHexadecimal: {hexa}')
+            self.senal_resultado((f'Decimal: {deci}', 'Hexadecimal: {hexa}'))
+
+        except ValueError as error:
+            print(error)
+
+    def hexadecimal(self, hexa: str) -> None:
+        try:
+            if (len(hexa) < 3) or (hexa[:2] != '0x' and hexa[:2] != '0X'):
+                raise ValueError('Hexadecimal inválido.')
+            
+            deci = transformador.hexadecimal_a_decimal(hexa)
+            bina = transformador.hexadecimal_a_binario(hexa)
+
+            print(f'\nDecimal: {deci}\nBinario: {bina}')
+            self.senal_resultado((f'Decimal: {deci}', 'Binario: {bina}'))
+      
+        except ValueError as error:
+            print(error)
 
 
 if __name__ == '__main__':
@@ -52,10 +70,10 @@ if __name__ == '__main__':
             print('Respuesta inválida.')
         elif respuesta == '1':
             numero = str(input('Ingrese el número: '))
-            decimal(numero)
+            Transformador().decimal(numero)
         elif respuesta == '2':
             numero = str(input('Ingrese el número: '))
-            binario(numero)
+            Transformador().binario(numero)
         elif respuesta == '3':
             numero = str(input('Ingrese el número: '))
-            hexadecimal(numero)
+            Transformador().hexadecimal(numero)
--- frontend/ventanas.py
+++ frontend/ventanas.py
@@ -1,10 +1,44 @@
 from sys import __excepthook__, exit
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QLineEdit, QComboBox, QPushButton, QHBoxLayout, QVBoxLayout
 
-class VentanaPrincial(QWidget):
+class VentanaPrincipal(QWidget):
+    senal_tipo_numero = pyqtSignal(str, str)
+    
     def __init__(self) -> None:
         super().__init__()
         self.setGeometry(500, 80, 400, 600)
+        self.label = QLabel('Ingrese su número a transformar: ', self)
+        self.input_num = QLineEdit('', self)
+        self.selector_numero = QComboBox(self)
+        self.selector_numero.addItems(['Decimal', 'Binario', 'Hexadecimal'])
+        self.boton_seleccionar = QPushButton('Seleccionar', self)
+        self.boton_seleccionar.clicked.connect(self.boton_presionado)
+        
+        hbox_ledit = QHBoxLayout()
+        hbox_cbox = QHBoxLayout()
+        vbox = QVBoxLayout()
+        
+        hbox_ledit.addStretch(1)
+        hbox_ledit.addWidget(self.label)
+        hbox_ledit.addWidget(self.input_num)
+        hbox_ledit.addStretch(1)
+        
+        hbox_cbox.addStretch(1)
+        hbox_cbox.addWidget(self.selector_numero)
+        hbox_cbox.addWidget(self.boton_seleccionar)
+        hbox_cbox.addStretch(1)
+
+        vbox.addLayout(hbox_ledit)
+        vbox.addLayout(hbox_cbox)
+        vbox.addStretch(1)
+        self.setLayout(vbox)
+
+    
+    def boton_presionado(self) -> None:
+        texto = self.selector_numero.currentText()
+        num = self.input_num.text()
+        self.senal_tipo_numero.emit(texto, num)
 
 
 if __name__ == '__main__':
@@ -14,6 +48,6 @@ if __name__ == '__main__':
     __excepthook__ = hook
 
     app = QApplication([])
-    ventana = VentanaPrincial()
+    ventana = VentanaPrincipal()
     ventana.show()
     exit(app.exec())
